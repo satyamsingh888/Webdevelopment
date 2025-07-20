ea4f29879c985a44297e8e8d9eb90e04015908ac
Satyam Singh <888hellboysatyam123@gmail.com>
2025-07-20 13:23:27 +0530

    learning

--- JavaScript/part2/dom/project/2_BMICalculator/chaiaurcode.js
+++ JavaScript/part2/dom/project/2_BMICalculator/chaiaurcode.js
@@ -1,2 +1,11 @@
 const form =  document.querySelector('form')
-// console.log(form);
+
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+    const  height = parseInt(document.querySelector('#height').value)  
+    const  weight = parseInt(document.querySelector('#weight').value)   
+    const  result = document.querySelector('#result')  
+
+
+    
+})
