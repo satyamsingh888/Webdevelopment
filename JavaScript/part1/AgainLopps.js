0fc4028ac161b943651fc6fc3fd10fbdbb339f5c
Satyam Singh <888hellboysatyam123@gmail.com>
2025-08-12 03:07:12 +0530

    learning

---
+++ JavaScript/part1/AgainLopps.js
@@ -0,0 +1,47 @@
+// // let i =1;
+// while(i<=15){
+//     if(i%2==1){
+//         // console.log(i);
+//     }
+//     i++;
+// }
+
+// for (let i = 1; i <= 15; i++) {
+//   if (i % 2 == 1) {
+//     // console.log(i);
+//   }
+// }
+
+// for(let i=1; i<=10; i++){
+//     // console.log(`5*${i} =`,5*i);
+// }
+
+// let sum = 0;
+// for (let i =1; i<=100; i++){
+//     sum = sum+i;
+// }
+// console.log(sum);
+
+
+// let sum =0;
+// let i =1;
+// while (i<=100) {
+//     sum = sum +i;
+//     i++;
+// }
+// console.log(sum);
+
+
+// for(let i = 1; i<=50; i++){
+//     if(i%3==0){
+//         console.log(i);
+//     }
+// }
+
+
+ for(let i=1; i<=20; i++){
+    if(i%3===0){
+        continue;
+    }
+    console.log(i);
+ }
