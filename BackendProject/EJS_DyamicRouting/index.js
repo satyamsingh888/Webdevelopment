29b6f3e0b457e78e6020d717882dc24bbcff3097
Satyam Singh <888hellboysatyam123@gamil.com>
2026-07-24 08:58:22 +0530

    learning

---
+++ BackendProject/EJS_DyamicRouting/index.js
@@ -0,0 +1,23 @@
+// const express = require('express');
+// const app = express();
+
+// app.use(express.json());
+// app.use(express.urlencoded({extended:true}));
+
+// app.use(express.static(path.join(__dirname,'public')))
+// app.set('view engine','ejs');
+
+
+// app.get('/',function(req,res){
+//     res.render('index')
+// });
+
+// app.get('/chacha',(req,res)=>{
+//  res.render('chacha')
+// })
+
+// app.listen(3000,function(){
+//     console.log("its running");
+// })
+
+console.log(__dirname+'/public');
