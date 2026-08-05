02c697fa4c53770213d3c7d33e4264af515692dd
Satyam Singh <888hellboysatyam123@gmail.com>
2026-08-05 16:25:29 +0530

    learning

--- BackendProject/EJS_DyamicRouting/index.js
+++ BackendProject/EJS_DyamicRouting/index.js
@@ -1,23 +1,27 @@
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
+const path = require('path');
 
-// app.use(express.json());
-// app.use(express.urlencoded({extended:true}));
+app.use(express.json());  //can be used form or can be handel data in backend 
+app.use(express.urlencoded({extended:true}));  //  same
 
-// app.use(express.static(path.join(__dirname,'public')))
-// app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname,'public')))  //can be used static file like images
+app.set('view engine','ejs');  //can be render ejs pages
 
 
-// app.get('/',function(req,res){
-//     res.render('index')
-// });
+app.get('/',function(req,res){
+    res.render('index')               // render pages and it is found in views folder
+});
 
-// app.get('/chacha',(req,res)=>{
-//  res.render('chacha')
-// })
 
-// app.listen(3000,function(){
-//     console.log("its running");
-// })
+app.get('/profile/:username',(req,res)=>{   //dynamic routing
+    res.send(`welcome: ${req.params.username}`)
+})
+app.get('/author/:username/:age',(req,res)=>{   //dynamic routing
+    res.send(`welcome: ${req.params.username}, ${req.params.age}`)
+})
 
-console.log(__dirname+'/public');
+app.listen(3000,function(){
+    console.log("its running");
+})
+
